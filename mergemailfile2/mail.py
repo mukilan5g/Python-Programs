5f765e5e5af1bd989c962bffedc6dac194d6bb5c
Nagarani.R <nagaraniraja9677@gmail.com>
2017-10-24 19:10:11 +0530

    merge mail file 2

---
+++ mergemailfile2/mail.py
@@ -0,0 +1,16 @@
+with open("names1.txt",'r') as names_file:
+
+
+   with open("body1.txt",'r') as body_file:
+   
+      
+       body = body_file.read()
+
+
+       for name in names_file:
+           mail = "Hello "+name+body
+
+         
+           with open(name.strip()+".txt",'w') as mail_file:
+               mail_file.write(mail)
+               
